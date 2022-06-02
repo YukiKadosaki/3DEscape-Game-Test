8cda9cdbdf70a914fd2e72f5a02a2ddd4f1d4576
YukiKadosaki <yuki1237@i.softbank.jp>
2022-06-03 06:40:11 +0900

    [Update]MenuManager

--- Assets/00_Assets/Scripts/MenuManager.cs
+++ Assets/00_Assets/Scripts/MenuManager.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    private const int canvas_width = 1920;
+    private const int canvas_height = 1080;
+
+
     public static MenuManager instance;
     private bool m_IsOpenning = false;
+
+    [SerializeField]
+    private GameObject m_Canvas;
+    [SerializeField]
+    private RectTransform m_MousePointer;
+    [SerializeField]
+    private float m_MouseSpeed = 50;//マウスの移動速度
+
+    private Vector2 m_MousePositionBefore;//前フレームのマウス座標の差を計るため
 
     private void Awake()
     {
@@ -20,12 +33,28 @@ public class MenuManager : MonoBehaviour
         }
     }
 
-    public void subroutine()
+    private void Start()
     {
-        Debug.Log("サブルーチンコール");
+        if (!m_Canvas)
+            Debug.LogError("インスペクターからキャンバスを設定してください");
+        if(!m_MousePointer)
+            Debug.LogError("インスペクターからマウスポインタ画像を設定してください");
+
+        m_Canvas.SetActive(false);
+        m_MousePositionBefore = Vector2.zero;
     }
 
     private void Update()
+    {
+        TryOpenAndCloseMenu();
+        
+        if (!m_IsOpenning)
+            return;
+
+        TryDrawMousePointer();
+    }
+
+    private void TryOpenAndCloseMenu()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
@@ -33,13 +62,42 @@ public class MenuManager : MonoBehaviour
             if (m_IsOpenning)
             {
                 Time.timeScale = 0;
-                Debug.Log("MenuOpen");
+                m_Canvas.SetActive(true);
             }
             else
             {
                 Time.timeScale = 1;
+                m_Canvas.SetActive(false);
             }
-            
+
         }
+    }
+
+    Vector2 mouseDeltaPosition;
+    float nextMouseX;
+    float nextMouseY;
+    Vector2 newPosition;
+
+    private void TryDrawMousePointer()
+    {
+        /*Vector2*/ mouseDeltaPosition = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        /*float*/ nextMouseX = m_MousePointer.position.x + mouseDeltaPosition.x * m_MouseSpeed;
+        /*float*/ nextMouseY = m_MousePointer.position.y + mouseDeltaPosition.y * m_MouseSpeed;
+        /*Vector2*/ newPosition = m_MousePositionBefore;
+
+        //Debug.Log(nextMouseX);
+        if (-1 <= nextMouseX && nextMouseX <= canvas_width)
+        {
+            newPosition.x = nextMouseX;
+        }
+
+        if(-1 <= nextMouseY && nextMouseY <= canvas_height)
+        {
+            newPosition.y = nextMouseY;
+        }
+        m_MousePointer.position = newPosition;
+
+        m_MousePositionBefore = newPosition;
+        Debug.Log(m_MousePointer.position);
     }
 }
