53c5105e0b5f48551a4ec9ee1ce2901416aa8e13
YukiKadosaki <yuki1237@i.softbank.jp>
2022-06-03 05:25:18 +0900

    [Update]Player
    Timescale <= 0のときに視点操作をできないようにした

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -236,7 +236,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void RotateView()
         {
-            m_MouseLook.LookRotation (transform, m_Camera.transform);
+            if(Time.timeScale >0)
+                m_MouseLook.LookRotation (transform, m_Camera.transform);
         }
 
 
